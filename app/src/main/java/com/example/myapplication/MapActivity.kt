338e56be2fbc12198f83959db2a0690fda5549bf
Alissa S. Cezero <144124273+lissCez@users.noreply.github.com>
2024-11-21 22:05:13 -0300

    feat: Updating

--- app/src/main/java/com/example/myapplication/MapActivity.kt
+++ app/src/main/java/com/example/myapplication/MapActivity.kt
@@ -5,16 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.databinding.ActivityMapBinding
 
 class MapActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMapBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_map)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
+        binding.mapView
     }
 }
